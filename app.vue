8ccdc63577d0b00968e27d19b193ca618ec0be25
didulanthaisuru <didulanthapki22@uom.lk>
2024-05-15 17:38:19 +0530

    new

--- app.vue
+++ app.vue
@@ -1,5 +1,39 @@
 <template>
+  <h1>{{ name }}</h1>
+
+  <div v-if="pending">Loading Projects...</div>
   <div>
-    <NuxtWelcome />
+    <h2>Projects</h2>
+    <ul>
+      <li v-for="project in projects" :key="project.name">
+        <h3>{{ project.name }}</h3>
+        <p>{{ project.description }}</p>
+      </li>
+    </ul>
+  </div>
+
+  <div v-if="pending1">Loading Blogs...</div>
+  <div>
+    <h2>Blogs</h2>
+    <ul>
+      <li v-for="blog in blogs">
+        <h3>{{ blog.title }}</h3>
+        <p>{{ blog.content }}</p>
+      </li>
+    </ul>
   </div>
 </template>
+
+<script setup>
+const name = "Isuru Didulantha";
+const {
+  data: projects,
+  pending,
+  error,
+} = useFetch("http://localhost:5000/projects");
+const {
+  data: blogs,
+  pending1,
+  error1,
+} = useFetch("http://localhost:5000/blogs");
+</script>
